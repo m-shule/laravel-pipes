32a29e4ccbb090b9e47696dd7c21830c9510d696
Naoray <krishan.koenig@gmail.com>
2019-06-05 17:48:43 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/PipeRequestTest.php
+++ tests/PipeRequestTest.php
@@ -360,7 +360,7 @@ class PipeRequestTest extends TestCase
     {
         Pipe::fake();
 
-        Pipe::namespace('Test')->group(__DIR__ . '/Fixtures/pipes.php');
+        Pipe::namespace('Test')->group(__DIR__.'/Fixtures/pipes.php');
 
         $this->pipe(['test' => 'ping']);
 
